a0960956e7601f5a6acaff09465b005c71fd62a2
Benoit DE COURSON <bdecourson@master1.frontex.ens.fr>
2022-03-01 17:27:27 +0100

    bla

--- try_m.py
+++ try_m.py
@@ -1,12 +1,14 @@
 import sys
 mu = float(sys.argv[1])
-m = float(sys.argv[2])
+#m = float(sys.argv[2])
+M = [10**-3,10**-2,.05,.1,.2]
 from model import Population
 import pickle
 import numpy as np
-P = Population(mu,m=m,initial_v=0,update_rate=.1,σ=4,n=20)
-P.round(50)
-np.save(f"result_{np.round(mu,2)}_m={m}_v=0.npy",P.frequencies)
-P = Population(mu,m=m,initial_v=1,update_rate=.1,σ=4,n=20)
-P.round(50)
-np.save(f"result_{np.round(mu,2)}_m={m}_v=1.npy",P.frequencies)
+for m in M:
+	P = Population(mu,m=m,initial_v=0,update_rate=.1,σ=4,n=20)
+	P.round(50)
+	np.save(f"Results/result_{np.round(mu,2)}_m={m}_v=0.npy",P.frequencies)
+	P = Population(mu,m=m,initial_v=1,update_rate=.1,σ=4,n=20)
+	P.round(50)
+	np.save(f"Results/result_{np.round(mu,2)}_m={m}_v=1.npy",P.frequencies)
