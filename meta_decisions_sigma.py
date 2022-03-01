866c062303728441553b0ac4732e1591fc17ceb2
regicid <57593826+regicid@users.noreply.github.com>
2022-03-01 12:54:21 +0100

    bla

--- meta_decisions_sigma.py
+++ meta_decisions_sigma.py
@@ -7,20 +7,21 @@ import pandas as pd
 prob_matrixes = prob_matrixer()
 a = dyn_prog(0,0,prob_matrixes)[0]
 
-M = [10**-3,10**-2,.05,.2]
+M = [4,8,12,16]
 V = np.linspace(.001,.999,50)
 P = [10**-4,.001,.003,.01,.1]
-Results = pd.DataFrame(index = np.arange(len(P)*len(V)*len(a)*len(M)),columns=("v","p","resources","action","f2"))
+Results = pd.DataFrame(index = np.arange(len(P)*len(V)*len(a)*len(M)),columns=("v","p","resources","action","sigma"))
 for k in range(len(M)):
+    prob_matrixes = prob_matrixer()
     for j in range(len(P)):
         for i in tqdm(range(len(V))):
-            fitness, exp,decisions = dyn_prog(p=P[j],v=V[i],prob_matrixes=prob_matrixes,f2 = M[k])
+            fitness, exp,decisions = dyn_prog(p=P[j],v=V[i],prob_matrixes=prob_matrixes,σ = M[k])
             slice_b = int(k*len(Results.index)/len(M) + (len(V)*j + i)*len(a))
             slice_e = int(k*len(Results.index)/len(M) + (len(V)*j + i+1)*len(a))
             Results.v[slice_b:slice_e] = V[i]
             Results.p[slice_b:slice_e] = P[j]
             Results.resources[slice_b:slice_e] = np.linspace(-50,50,1001)
             Results.action[slice_b:slice_e] = decisions
-            Results.f2[slice_b:slice_e] = M[k]
+            Results.sigma[slice_b:slice_e] = M[k]
 
 np.save("Results/decisions_f.npy",Results)
