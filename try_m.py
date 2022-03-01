4650f04879487de45943e563b73b28815e7a66b0
Benoit DE COURSON <bdecourson@master1.frontex.ens.fr>
2022-03-01 23:28:41 +0100

    bla

--- try_m.py
+++ try_m.py
@@ -9,6 +9,6 @@ for m in M:
 	P = Population(mu,m=m,initial_v=0,update_rate=.1,σ=4,n=20,r=.85)
 	P.round(50)
 	np.save(f"Results/result_{np.round(mu,2)}_m={m}_v=0.npy",P.frequencies)
-	P = Population(mu,m=m,initial_v=1,update_rate=.1,σ=4,n=20)
+	P = Population(mu,m=m,initial_v=1,update_rate=.1,σ=4,n=20,r=.85)
 	P.round(50)
 	np.save(f"Results/result_{np.round(mu,2)}_m={m}_v=1.npy",P.frequencies)
