d01a6151232a8099f9babb707e2dbd93c1f2b073
Benoit DE COURSON <bdecourson@master1.frontex.ens.fr>
2022-03-03 16:55:31 +0100

    bla

--- meta_m.py
+++ meta_m.py
@@ -3,9 +3,8 @@ import numpy as np
 import pickle
 import subprocess
 import time
-PARAM = np.linspace(5,14,80)
-M = [10**-3,10**-2,.05,.1,.2]
+PARAM = np.linspace(5,14,20)
+M = [10**-2,.05]
 for i in PARAM:
 	bash = "srun -N 1 -o logs.out --partition=firstgen,dellgen python3 /home/bdecourson/model/try_m.py " + str(i)
 	subprocess.Popen(bash.split(),stdout=subprocess.PIPE)	
-	time.sleep(.5)
--- try_m.py
+++ try_m.py
@@ -7,8 +7,8 @@ import pickle
 import numpy as np
 for m in M:
 	P = Population(mu,m=m,initial_v=0,update_rate=.1,σ=4,n=20,r=.85)
-	P.round(50)
+	P.round(1000)
 	np.save(f"Results/result_{np.round(mu,2)}_m={m}_v=0.npy",P.frequencies)
 	P = Population(mu,m=m,initial_v=1,update_rate=.1,σ=4,n=20,r=.85)
-	P.round(50)
+	P.round(1000)
 	np.save(f"Results/result_{np.round(mu,2)}_m={m}_v=1.npy",P.frequencies)
