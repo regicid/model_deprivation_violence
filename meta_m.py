393dd1be76a4a3475fe99aaae64b3e4a3506a29b
Benoit DE COURSON <bdecourson@master1.frontex.ens.fr>
2022-03-07 16:19:25 +0100

    bla

--- meta_m.py
+++ meta_m.py
@@ -3,8 +3,9 @@ import numpy as np
 import pickle
 import subprocess
 import time
-PARAM = np.linspace(5,14,20)
-M = [.1,.25]
+PARAM = np.linspace(5,14,100)
+M = [.001,.01,.05]
 for i in PARAM:
-	bash = "srun -N 1 -o logs.out --partition=firstgen,dellgen python3 /home/bdecourson/model/try_m.py " + str(i)
+	bash = "srun -N 1 -o logs.out --partition=dellgen python3 /home/bdecourson/model/try_m.py " + str(i)
 	subprocess.Popen(bash.split(),stdout=subprocess.PIPE)	
+	time.sleep(2)
