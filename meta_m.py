0812fd3adc212a5978de2262b1e2236b8c615edd
Benoit DE COURSON <bdecourson@master1.frontex.ens.fr>
2022-03-08 19:33:07 +0100

    bla

--- meta_m.py
+++ meta_m.py
@@ -4,7 +4,7 @@ import pickle
 import subprocess
 import time
 PARAM = np.linspace(5,14,100)
-M = [.001,.01,.05]
+M = [,.01,.05]
 for i in PARAM:
 	bash = "srun -N 1 -o logs.out --partition=dellgen,firstgen python3 /home/bdecourson/model/try_m.py " + str(i)
 	subprocess.Popen(bash.split(),stdout=subprocess.PIPE)	
