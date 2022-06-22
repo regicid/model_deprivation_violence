c1bcacdaeeabed1dedc2185698e08b1ee461e7a2
Benoit DE COURSON <bdecourson@master1.frontex.ens.fr>
2022-06-22 19:49:24 +0200

    bla

--- try_m.py
+++ try_m.py
@@ -5,9 +5,9 @@ from model import Population
 import pickle
 import numpy as np
 for m in M:
-	P = Population(mu,m=m,initial_v=0,update_rate=.1,σ=4,r=.99,n=50,π = 10,β=5)
+	P = Population(mu,m=m,initial_v=0,σ=6,update_rate = .1)
 	P.round(100)
 	np.save(f"Results/result2_{np.round(mu,2)}_m={m}_v=0.npy",P.frequencies)
-	P = Population(mu,m=m,initial_v=1,update_rate=.1,σ=4,r=.99,n=50,π = 10,β=5)
+	P = Population(mu,m=m,initial_v=1,σ=6,update_rate = .1)
 	P.round(100)
 	np.save(f"Results/result2_{np.round(mu,2)}_m={m}_v=1.npy",P.frequencies)
