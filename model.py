c8a02cd6c4af04a9c28da32ed929972e0139706b
regicid <57593826+regicid@users.noreply.github.com>
2022-09-20 17:55:13 +0200

    bla

--- model.py
+++ model.py
@@ -69,14 +69,14 @@ class Population:
         self.strategies = strategies[positions]
         self.p = np.mean(self.strategies==2)
         self.v = initial_v
-        self.update_strategies()
+        self.update_strategies(update_rate=1)
         #z = self.strategies < 2
         #zz = np.random.random(z.sum()) < initial_v
         #self.strategies[z] = zz
     def measure(self):
         self.p = np.mean(self.strategies==2)
         self.v = np.mean(self.strategies>0)
-    def update_strategies(self):    
+    def update_strategies(self,update_rate = self.update_rate):    
         p = (1-(1-self.p)**self.n)/self.n
         if self.v==1: 
             v=.999
@@ -100,7 +100,7 @@ class Population:
             decisions = np.argmax(exp_fitness,axis=0)
             fitness = np.max(exp_fitness,axis=0)
         strategies = decisions
-        z = np.random.random(self.N)<self.update_rate
+        z = np.random.random(self.N)<update_rate
         positions = ((self.states[z]-self.state_space.min())/(self.state_space[1]-self.state_space[0])).round().astype('int')
         self.strategies[z] = strategies[positions]
     def round(self,t):
