d32c7eef950ac0094b9beca698c6d5da74b5119f
regicid <57593826+regicid@users.noreply.github.com>
2022-10-13 11:17:00 +0200

    Rename lambda

--- decisions.py
+++ decisions.py
@@ -20,7 +20,7 @@ def prob_matrixer(μ =10,r=.99,σ=4,β=10,π=20,state_space = np.round(np.linspa
 
 
 
-def dyn_prog(p,v,prob_matrixes,T = 200,n=10,r=.99,γ=1/3,m=.01,f2=.05,ω=.1,β=10,π=20,state_space = np.round(np.linspace(-50,50,1001),1)):
+def dyn_prog(p,v,prob_matrixes,T = 200,n=10,γ=1/3,m=.01,λ=.05,ω=.1,β=10,π=20,state_space = np.round(np.linspace(-50,50,1001),1)):
     p = (1-(1-p)**n)/n
     if v==1: v=.999
     fitness = (100+state_space)/2
@@ -37,7 +37,7 @@ def dyn_prog(p,v,prob_matrixes,T = 200,n=10,r=.99,γ=1/3,m=.01,f2=.05,ω=.1,β=1
         temp_fitness = []
         exp_fitness = np.empty(shape=(3,len(state_space)),dtype = "float")
         for strat in range(3):
-            exp_fitness[strat,:] = np.dot(outcomes[strat],fitness*(1-ω*(state_space<0))*(1 - prob_fight[strat]*f2/2))
+            exp_fitness[strat,:] = np.dot(outcomes[strat],fitness*(1-ω*(state_space<0))*(1 - prob_fight[strat]*λ/2))
         decisions = np.argmax(exp_fitness,axis=0)
         fitness = np.max(exp_fitness,axis=0)
     return fitness, exp_fitness, decisions
