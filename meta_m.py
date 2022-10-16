3b645404ec1bbe92bed86835cd917347dfd33ac4
regicid <57593826+regicid@users.noreply.github.com>
2022-10-16 19:34:20 +0200

    readme

--- meta_m.py
+++
@@ -1,9 +0,0 @@
-import os
-import numpy as np
-import pickle
-import subprocess
-import time
-PARAM = np.linspace(5,14,100)
-for i in PARAM:
-	bash = "srun -N 1 -o logs.out --partition=dellgen,firstgen python3 /home/bdecourson/model/try_m.py " + str(i)
-	subprocess.Popen(bash.split(),stdout=subprocess.PIPE)	
