7cebc445133fec3d9c6c1823a7d515e37471d55a
Benoit DE COURSON <bdecourson@master1.frontex.ens.fr>
2022-12-13 16:26:16 +0100

    bla

--- try_sigma.py
+++ try_sigma.py
@@ -9,5 +9,5 @@ import numpy as np
 for n in N:
 	for initial_v in [0,1]:
 		P = Population(mu,σ=sigma,m=m,λ=.1,initial_v=initial_v,update_rate=.1,r=.99,n=n)
-		P.round(100)
+		P.round(200)
 		np.save(f"Results/result3_sigma={np.round(sigma,2)}_n={n}_v={initial_v}.npy",P.frequencies)
