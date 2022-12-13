d99044523f5efbb7fa89f2719800572643bbf24e
Benoit DE COURSON <bdecourson@master1.frontex.ens.fr>
2022-12-13 12:29:30 +0100

    bla

--- try_sigma.py
+++ try_sigma.py
@@ -8,6 +8,6 @@ import pickle
 import numpy as np
 for n in N:
 	for initial_v in [0,1]:
-		P = Population(mu,σ=sigma,m=m,initial_v=initial_v,update_rate=.1,r=.99,n=n)
+		P = Population(mu,σ=sigma,m=m,λ=.1,initial_v=initial_v,update_rate=.1,r=.99,n=n)
 		P.round(100)
-		np.save(f"Results/result2_sigma={np.round(sigma,2)}_n={n}_v={initial_v}.npy",P.frequencies)
+		np.save(f"Results/result3_sigma={np.round(sigma,2)}_n={n}_v={initial_v}.npy",P.frequencies)
