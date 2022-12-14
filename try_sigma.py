418905aaa9329951d6bb35fcae65994d8a3d9ffe
Benoit DE COURSON <bdecourson@master1.frontex.ens.fr>
2022-12-14 10:35:52 +0100

    bla

--- try_sigma.py
+++ try_sigma.py
@@ -3,11 +3,11 @@ mu = 15
 sigma = float(sys.argv[1])
 N = [50]
 m = .2
-from model2 import Population
+from model import Population
 import pickle
 import numpy as np
 for n in N:
 	for initial_v in [0,1]:
-		P = Population(mu,σ=sigma,m=m,f2=.05,β=5,π=10,initial_v=initial_v,update_rate=.1,r=.99,n=n)
+		P = Population(mu,σ=sigma,m=m,γ=.05,β=5,π=10,initial_v=initial_v,update_rate=.1,r=.99,n=n)
 		P.round(200)
 		np.save(f"Results/result4_sigma={np.round(sigma,2)}_n={n}_v={initial_v}.npy",P.frequencies)
